d028ed0c735169e3a18e8135557b5630d0386615
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:06 -0800

    tests

--- yup-wc/main.go
+++ yup-wc/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/wc"
 )
 
@@ -69,7 +69,7 @@ func action(c *cli.Context) error {
 
 	// Add file arguments (or none for stdin)
 	for i := 0; i < c.NArg(); i++ {
-		params = append(params, yup.File(c.Args().Get(i)))
+		params = append(params, gloo.File(c.Args().Get(i)))
 	}
 
 	// Add flags based on CLI options
@@ -91,5 +91,5 @@ func action(c *cli.Context) error {
 
 	// Create and execute the wc command
 	cmd := Wc(params...)
-	return yup.Run(cmd)
+	return gloo.Run(cmd)
 }
